c01b2644747f4a942fac19e20e42847ac0492dde
Vojtech Stursa <vojtech.stursa@firma.seznam.cz>
2025-10-08 16:16:10 +0200

    update to 1.0.1

--- Formula/yamltool.rb
+++ Formula/yamltool.rb
@@ -1,8 +1,8 @@
 class Yamltool < Formula
   desc "Command-line utility for reading, querying, and manipulating YAML files"
   homepage "https://gitlab.seznam.net/vojtech.stursa/tool_yamltool"
-  url "https://gitlab.seznam.net/vojtech.stursa/tool_yamltool/-/archive/v1.0.0/tool_yamltool-v1.0.0.tar.gz"
-  sha256 "3f450243bad5b1b9bec2f67fa39fc5647f571d26c3fd618a04ff7305807e0f6e" # Will be filled after creating the release
+  url "https://gitlab.seznam.net/vojtech.stursa/tool_yamltool/-/archive/v1.0.1/tool_yamltool-v1.0.1.tar.gz"
+  sha256 "1f30fe11d53a3dd4ff565612419f3c5c6220264833d6a93742eda367d7e040ad" # Will be filled after creating the release
   license "MIT"
   head "https://gitlab.seznam.net/vojtech.stursa/tool_yamltool.git", branch: "main"
 
@@ -28,6 +28,6 @@ class Yamltool < Formula
     assert_match "Test", output
 
     # Test version
-    assert_match "1.0.0", shell_output("#{bin}/yamltool --version")
+    assert_match "1.0.1", shell_output("#{bin}/yamltool --version")
   end
 end
