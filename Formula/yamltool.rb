1883b8edc67ed3ba0ed6dd555d8a44ea3a599c14
Asistent Bohus <ios@firma.seznam.cz>
2025-10-11 12:35:29 +0200

    Update yamltool to version v1.0.25 (universal binaries for ARM64 and x86_64)

--- Formula/yamltool.rb
+++ Formula/yamltool.rb
@@ -1,16 +1,16 @@
 class Yamltool < Formula
   desc "Command-line utility for reading, querying, and manipulating YAML files"
-  homepage "https://gitlab.seznam.net/vojtech.stursa/tool_yamltool"
+  homepage "https://gitlab.seznam.net/vojtech.stursa/tool_yamltool.git"
   license "MIT"
   head "https://gitlab.seznam.net/vojtech.stursa/tool_yamltool.git", branch: "main"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://sbrowser.dev.dnsz.cz/vojtech.stursa/artefacts/yamltool/yamltool-v1.0.5-arm64-apple-darwin.tar.gz"
-      sha256 "52fe959642a6a4bd434aae14321d6851a117ad1a18c010e099f5576f1bac6766"
+      url "https://sbrowser.dev.dnsz.cz/vojtech.stursa/artefacts/yamltool/yamltool-v1.0.25-arm64-apple-darwin.tar.gz"
+      sha256 "c970e0fbc92d05019f6781f066ce06930ea73ae2ba2c58eed4bab5ea5a15d354"
     else
-      url "https://sbrowser.dev.dnsz.cz/vojtech.stursa/artefacts/yamltool/yamltool-v1.0.5-x86_64-apple-darwin.tar.gz"
-      sha256 "025c4c7f2ec762651ca8cc2868f3aabf77fe9bb48790540e4e3597cfdd92c980"
+      url "https://sbrowser.dev.dnsz.cz/vojtech.stursa/artefacts/yamltool/yamltool-v1.0.25-x86_64-apple-darwin.tar.gz"
+      sha256 "58dcebf15295a2bad734cafcbe3ad4b5c359ac896de9de4846aebceaf53a398a"
     end
   end
 
@@ -33,6 +33,6 @@ class Yamltool < Formula
     assert_match "Test", output
 
     # Test version
-    assert_match "v1.0.5", shell_output("#{bin}/yamltool --version")
+    assert_match "v1.0.25", shell_output("#{bin}/yamltool --version")
   end
 end
